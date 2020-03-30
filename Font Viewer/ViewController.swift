c7db437453d5e016be3f3a2a5bdd5e05e10d7039
harshithpabbati <pabbatiharshith@gmail.com>
2020-03-30 20:35:08 +0530

    Display selected Font Family and Font Types

--- Font Viewer/ViewController.swift
+++ Font Viewer/ViewController.swift
@@ -7,8 +7,79 @@
 //
 
 import Cocoa
+ 
 
 class ViewController: NSViewController {
+    
+    func setupUI() {
+        fontFamiliesPopup.removeAllItems()
+        fontTypesPopup.removeAllItems()
+        sampleLabel.stringValue = ""
+        sampleLabel.alignment = .center
+    }
+    
+    func populateFontFamilies() {
+        fontFamiliesPopup.removeAllItems()
+        fontFamiliesPopup.addItems(withTitles: NSFontManager.shared.availableFontFamilies)
+        handleFontFamilySelection(self)
+    }
+    
+    func updateFontTypesPopup() {
+        fontTypesPopup.removeAllItems()
+     
+        for member in fontFamilyMembers {
+            if let fontType = member[1] as? String {
+                fontTypesPopup.addItem(withTitle: fontType)
+            }
+        }
+        fontTypesPopup.selectItem(at: 0)
+        handleFontTypeSelection(self)
+    }
+    
+    override func viewWillAppear() {
+        super.viewWillAppear()
+     
+        setupUI()
+        populateFontFamilies()
+        view.window?.title = "Fonts"
+    }
+    var selectedFontFamily: String?
+
+    var fontFamilyMembers = [[Any]]()
+        
+    @IBOutlet weak var fontFamiliesPopup: NSPopUpButton!
+     
+    @IBOutlet weak var fontTypesPopup: NSPopUpButton!
+     
+    @IBOutlet weak var sampleLabel: NSTextField!
+    
+    @IBAction func handleFontFamilySelection(_ sender: Any) {
+        if let fontFamily = fontFamiliesPopup.titleOfSelectedItem {
+        
+               selectedFontFamily = fontFamily
+        
+               if let members = NSFontManager.shared.availableMembers(ofFontFamily: fontFamily) {
+                   fontFamilyMembers.removeAll()
+                   fontFamilyMembers = members
+                   updateFontTypesPopup()
+               }
+           }
+    }
+     
+     
+    @IBAction func handleFontTypeSelection(_ sender: Any) {
+        let selectedMember = fontFamilyMembers[fontTypesPopup.indexOfSelectedItem]
+     
+        if let weight = selectedMember[2] as? Int, let traits = selectedMember[3] as? UInt, let fontfamily = selectedFontFamily {
+     
+            let font = NSFontManager.shared.font(withFamily: fontfamily,
+                                                 traits: NSFontTraitMask(rawValue: traits),
+                                                 weight: weight,
+                                                 size: 40.0)
+            sampleLabel.font = font
+            sampleLabel.stringValue = "Hello World!"
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ README.md
@@ -0,0 +1,3 @@
+# Font Viewer
+
+![Font Viewer](https://user-images.githubusercontent.com/43822585/77928872-9b21dc00-72c6-11ea-9d37-b0c9dc631d03.png)
