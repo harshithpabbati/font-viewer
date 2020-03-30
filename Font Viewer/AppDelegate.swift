276809db99631e2129eaf76229254a281f1b6615
harshithpabbati <pabbatiharshith@gmail.com>
2020-03-30 19:46:05 +0530

    Initial Commit

---
+++ Font Viewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Font Viewer
+//
+//  Created by Harshith on 30/03/20.
+//  Copyright © 2020 Harshith. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Font Viewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Font Viewer
+//
+//  Created by Harshith on 30/03/20.
+//  Copyright © 2020 Harshith. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
